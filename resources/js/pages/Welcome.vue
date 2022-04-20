b586a7347502388d70322894a29c1ceee91bdc45
locvv <locvv@yopaz.vn>
2022-04-20 17:13:15 +0700

    add inertia

---
+++ resources/js/pages/Welcome.vue
@@ -0,0 +1,13 @@
+<template lang="">
+  <div>
+    v-preasdasasd asd
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="">
+  
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .vue()
-    .sass('resources/sass/app.scss', 'public/css');
+    .vue(3)
+    .sass('resources/sass/app.scss', 'public/css')
+
+    .version();
